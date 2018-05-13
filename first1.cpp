b73bffca3034346c8cc3b753f2a1960b1da53d42
Your Name <reazulislam137@gmail.com>
2018-05-13 23:54:07 +0600

    first commit

---
+++ first1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+int main()
+{
+    cout << "This is line 1. " << endl;
+    cout << "This is line 2. " << endl;
+    cout << "This is line 3. " << endl;
+
+    cout << "World1"<< endl << "World2" << endl << "World3" << endl;
+
+    return 0;
+}
